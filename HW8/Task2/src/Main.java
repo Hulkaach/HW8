092a5b5455699d44ecbb7ef18bcab75f47a3076c
DESKTOP-QQACB3T\Chester <zolinanata2006@mail.ru>
2023-11-29 20:20:54 +0300

    done

--- HW8/Task2/src/Main.java
+++ HW8/Task2/src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         //todo код программы писать тут
+        int countPeople = 10;
+        double mediumWeight = 0;
+        int hevyWeightCount = 0;
+        int [] weightPeople = new int[countPeople];
+        for (int i = 0; i < weightPeople.length; i++) {
+            weightPeople[i]=new Random().nextInt(40,100);
+            mediumWeight+=weightPeople[i];
+            if (weightPeople[i]>=60 && weightPeople[i]<=80){
+                hevyWeightCount++;
+            }
+            System.out.print(" "+weightPeople[i]);
+        }
+        System.out.println("\n*****");
+        System.out.println("Средний вес "+countPeople + " людей составляет: "+mediumWeight/weightPeople.length+ " кг");
+        System.out.println("Количество людей у которых вес находится в промежутке от 60 до 80 кг составвляет: "+hevyWeightCount+ " человек(а)");
     }
 }
